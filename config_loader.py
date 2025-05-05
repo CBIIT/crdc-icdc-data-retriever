51bcab5ef1a79389ba1a40bf3c84d108bae2c84d
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-05 12:54:08 -0400

    add draft of YAML validator

--- config_loader.py
+++ config_loader.py
@@ -20,3 +20,34 @@ class ConfigHandler:
             raise ValueError("Missing 'project' key in config")
         if "sources" not in self.config or not isinstance(self.config["sources"], list):
             raise ValueError("Missing or invalid 'sources' section in config")
+        for source in self.config["sources"]:
+            if (
+                "name" not in source or 
+                "type" not in source or 
+                "api_base_url" not in source or
+                ("endpoint" not in source and "discovery" not in source)
+            ):
+                raise ValueError("Each data source must define a 'name', 'type', 'api_base_url' and 'endpoint'")
+            if (
+                source["type"] == "graphql" and
+                (("query" not in source or "method" not in source) or
+                source["method"].lower() != "post") 
+            ):
+                raise ValueError("GraphQL type sources require a 'query' and POST 'method'")
+            if "discovery" in source and "fetch" not in source:
+                raise ValueError("'fetch' property undefined for source requiring 'discovery'")
+            if (
+                "discovery" in source and
+                "endpoint" not in source["discovery"] or
+                "match_key" not in source["discovery"] or
+                "filter_prefix" not in source["discovery"]
+            ):
+                raise ValueError("'discovery' property requires 'endpoint', 'match_key' and 'filter_prefix'")
+            if (
+                "discovery" in source and
+                "endpoint_template" not in source["fetch"] or
+                "key_param" not in source["fetch"]
+            ):
+                raise ValueError("'fetch' property requires 'endpoint_template' and 'key_param'")
+            
+        return

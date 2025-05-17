8392f0aa40a01292085ac4b1d84ba943aa0513d2
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-16 21:58:09 -0400

    fuzzy match study/collection names

--- processor/mapper.py
+++ processor/mapper.py
@@ -1,3 +1,6 @@
+from utils.match_utils import is_fuzzy_match
+
+
 def map_matches_to_study(
     study,
     matched_source_data,
@@ -8,14 +11,18 @@ def map_matches_to_study(
     post_processor=None,
 ) -> list:
     crdc_links = []
+    study_name = study.get("clinical_study_designation", "")
 
     for metadata in matched_source_data:
+        candidate = metadata.get(match_key, "")
+        if not is_fuzzy_match(study_name, candidate):
+            continue
+
         if post_processor:
             metadata = post_processor(metadata)
 
         match_id = metadata.get(match_key)
         url = dataset_base_url.format(**{dataset_base_url_param: match_id})
-
         crdc_links.append(
             {"repository": repository_name, "url": url, "metadata": metadata}
         )

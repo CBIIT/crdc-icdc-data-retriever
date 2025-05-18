4cd50b7ac5321808b5f8d095f23412d47d21394a
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-18 18:53:26 -0400

    add mapping collector + generalize names

--- processor/mapper.py
+++ processor/mapper.py
@@ -1,8 +1,8 @@
 from utils.match_utils import is_fuzzy_match
 
 
-def map_matches_to_study(
-    study,
+def map_matches_to_entity(
+    entity,
     matched_source_data,
     dataset_base_url,
     dataset_base_url_param,
@@ -11,11 +11,12 @@ def map_matches_to_study(
     post_processor=None,
 ) -> list:
     crdc_links = []
-    study_name = study.get("clinical_study_designation", "")
+    entity_name = entity.get("clinical_study_designation", "")
 
     for metadata in matched_source_data:
         candidate = metadata.get(match_key, "")
-        if not is_fuzzy_match(study_name, candidate):
+        # log warning if match key not present in metadata
+        if not is_fuzzy_match(entity_name, candidate):
             continue
 
         if post_processor:
@@ -28,3 +29,42 @@ def map_matches_to_study(
         )
 
     return crdc_links
+
+
+def collect_mappings(
+    entities,
+    matched_source_data,
+    dataset_base_url,
+    dataset_base_url_param,
+    repository_name,
+    match_key,
+    post_processor=None,
+) -> list:
+    crdc_mappings = []
+
+    for entity in entities:
+        mappings = map_matches_to_entity(
+            entity=entity,
+            matched_source_data=matched_source_data,
+            dataset_base_url=dataset_base_url,
+            dataset_base_url_param=dataset_base_url_param,
+            repository_name=repository_name,
+            match_key=match_key,
+            post_processor=post_processor,
+        )
+
+        if mappings:
+            crdc_mappings.append(
+                {
+                    "CRDCLinks": mappings,
+                    "number_of_crdc_nodes": entity.get(
+                        "numberOfCRDCNodes", len({i["repository"] for i in mappings})
+                    ),
+                    "number_of_collections": entity.get(
+                        "numberOfImageCollections", len(mappings)
+                    ),
+                    "entity_id": entity.get("clinical_study_designation"),
+                }
+            )
+
+    return crdc_mappings

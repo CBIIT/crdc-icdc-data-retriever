9699e43f0abcf2e4ed54fe82a377004fdf921fde
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-21 18:58:58 -0400

    refactor dispatcher

--- core/dispatcher.py
+++ core/dispatcher.py
@@ -3,21 +3,61 @@ from processor.mapper import collect_mappings
 from processor.post_processor_registry import get_post_processor
 
 
+# def run_dispatcher(config):
+#     entity_source_name = config["entity_source"]
+#     sources = config["sources"]
+
+#     # fetch phase
+#     fetched_data = {}
+#     for source in sources:
+#         source_name = source["name"]
+#         fetched_data[source_name] = fetch_from_source(config)
+
+#     # entity_source = next(src for src in sources if src["name"] == entity_source_name)
+#     entities = fetched_data[entity_source_name]
+
+#     # match phase
+#     results = []
+#     for source in sources:
+#         if source["name"] == entity_source_name:
+#             continue
+
+#         source_data = fetched_data[source["name"]]
+#         post_processor = get_post_processor(source.get("post_processor"))
+
+#         mappings = collect_mappings(
+#             entities=entities,
+#             source_config=source,
+#             matched_source_data=source_data,
+#             dataset_base_url=source["dataset_base_url"],
+#             dataset_base_url_param=source["dataset_base_url_param"],
+#             repository_name=source["name"],
+#             match_key=source["match_key"],
+#             post_processor=post_processor,
+#         )
+
+#         if mappings:
+#             results.extend(mappings)
+
+#     return results
+
+
 def run_dispatcher(config):
-    entity_source_name = config["entity_source"]
+    entity_source_name = config["entity_source_name"]
     sources = config["sources"]
 
-    # fetch phase
-    fetched_data = {}
-    for source in sources:
-        source_name = source["name"]
-        fetched_data[source_name] = fetch_from_source(config)
-
-    # entity_source = next(src for src in sources if src["name"] == entity_source_name)
+    fetched_data = fetch_all(sources)
     entities = fetched_data[entity_source_name]
+    return match_all(entities, sources, fetched_data, entity_source_name)
 
-    # match phase
+
+def fetch_all(sources: list) -> dict:
+    return {source["name"]: fetch_from_source(source) for source in sources}
+
+
+def match_all(entities, sources, fetched_data, entity_source_name) -> list:
     results = []
+
     for source in sources:
         if source["name"] == entity_source_name:
             continue

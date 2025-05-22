2c23cf0678ca56d3480cef20669498141f5570c4
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-22 18:12:23 -0400

    naming consistency

--- processor/post_processor.py
+++ processor/post_processor.py
@@ -8,7 +8,6 @@ def post_processor(fn):
 
 
 def transform_html(html: str) -> str:
-    # Python version of node.js getIdcCollectionMetadata
     # handle oddly-formatted IDC response HTML
     converter = HTML2Text()
     converter.ignore_links = True
@@ -30,15 +29,14 @@ def clean_idc_metadata(metadata: dict) -> dict:
 
 
 @post_processor
-def aggregate_tcia_series_data(data: list, study: str, collection_id: str) -> dict:
-    # Python version of node.js getTciaCollectionMetadata
+def aggregate_tcia_series_data(data: list, entity: str, collection_id: str) -> dict:
     total_images = sum(int(i["ImageCount"]) for i in data)
     total_patients = len(set([i["PatientID"] for i in data]))
     unique_modalities = list(set([i["Modality"] for i in data]))
     unique_bodyparts = list(set([i["BodyPartExamined"] for i in data]))
 
     # hardcode inaccessible TCIA data for GLIOMA01
-    if study == "GLIOMA01":
+    if entity == "GLIOMA01":
         unique_modalities.append("Histopathology")
         total_images += 84
 

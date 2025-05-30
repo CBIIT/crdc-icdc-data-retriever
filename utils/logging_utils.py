b528fa20c66d1b936040b65be99ebe121f846a9c
Eric Miller <ermiller@alumni.cmu.edu>
2025-05-30 19:31:01 -0400

    size limits on log files

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,18 +1,24 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 
 
 def setup_logging(log_file_path="tmp/app.log", level=logging.INFO):
     os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
-    file_handler = logging.FileHandler(log_file_path)
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=5_000_000, backupCount=3)
     file_handler.setFormatter(formatter)
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(level)
-    root_logger.handlers = [console_handler, file_handler]
+    logger.addHandler(file_handler)

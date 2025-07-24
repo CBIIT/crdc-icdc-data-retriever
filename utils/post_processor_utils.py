ea0c3428092fc5c3f643458f8a113d0089562a8e
Eric Miller <ermiller@alumni.cmu.edu>
2025-07-24 14:36:13 -0400

    udpate deep merge util

--- utils/post_processor_utils.py
+++ utils/post_processor_utils.py
@@ -1,23 +1,23 @@
-def deep_merge_additive(d1: dict, d2: dict) -> dict:
-    """Recursively merges two dictionaries (d2 -> d1).
+def deep_merge_additive(base: dict, override: dict) -> dict:
+    """Recursively merges two dictionaries (override -> base).
 
     Args:
-        d1 (dict): The first dictionary.
-        d2 (dict): The second dictionary.
+        base (dict): The base dictionary.
+        override (dict): The override dictionary.
 
     Returns:
-        dict: Modifies d1 dict in place and returns it.
+        dict: Modifies base dict in place and returns it.
     """
-    for k, v in d2.items():
-        if k in d1:
-            if isinstance(d1[k], dict) and isinstance(v, dict):
-                deep_merge_additive(d1[k], v)
-            elif isinstance(d1[k], list) and isinstance(v, list):
-                d1[k].extend(v)
-            elif isinstance(d1[k], int) and isinstance(v, int):
-                d1[k] += v
+    for k, v in override.items():
+        if k in base:
+            if isinstance(base[k], dict) and isinstance(v, dict):
+                deep_merge_additive(base[k], v)
+            elif isinstance(base[k], list) and isinstance(v, list):
+                base[k] = sorted(set(base[k] + v))
+            elif isinstance(base[k], int) and isinstance(v, int):
+                base[k] += v
             else:
-                d1[k] = v
+                base[k] = v
         else:
-            d1[k] = v
-    return d1
+            base[k] = v
+    return base
